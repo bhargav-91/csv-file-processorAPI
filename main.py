fbb34f4d3976c87e77cdb52a79f344f88e15f135
Bhargav <bhargav.chakravarthy@gmail.com>
2022-07-14 21:43:01 +0530

    final commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from tempfile import NamedTemporaryFile
+import os
+from fastapi.middleware.cors import CORSMiddleware
+import csv
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/watch-dog-scan-report")
+async def upload():
+    data = []
+    try:
+        with open('/Users/bhargj/worksapce/SG-Hackverse/test-file/blockchain_watchdog.csv', 'r') as csvf:
+            csvReader = csv.DictReader(csvf)
+            for rows in csvReader:
+                print('The value of ROW Is : ',rows)
+                data.append(rows)
+                
+
+    finally:
+        print("process done")
+    
+    return data
